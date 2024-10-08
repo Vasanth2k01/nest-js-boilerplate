a9b2ceace117880bcf4567848faa1d06ff780150
Vasanth2k01 <vasanht2001@gmail.com>
2024-10-08 08:41:57 +0530

    (feat): Unit test added

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,20 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
 
 describe('PostsController', () => {
   let controller: PostsController;
+
+  const mockPostsService = {
+    create: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+    update: jest.fn().mockImplementation((id, dto) => ({
+      id,
+      ...dto,
+    })),
+    remove: jest.fn().mockImplementation((id) => ({ id })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
       providers: [PostsService],
-    }).compile();
+    })
+      .overrideProvider(PostsService)
+      .useValue(mockPostsService)
+      .compile();
 
     controller = module.get<PostsController>(PostsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create post', () => {
+    const dto = { title: 'Vasanth' };
+    expect(controller.create(dto)).toEqual({
+      id: expect.any(Number),
+      title: 'Vasanth',
+    });
+
+    expect(mockPostsService.create).toHaveBeenCalledWith(dto);
+  });
+
+  it('should update a post', () => {
+    const dto = { title: 'Vasanth' };
+    expect(controller.update('1', dto)).toEqual({
+      id: 1,
+      ...dto,
+    });
+
+    expect(mockPostsService.update).toHaveBeenCalled();
+  });
+
+  it('should delete a post', () => {
+    const dto = { id: '1' };
+
+    // jest.spyOn(mockPostsService, 'remove').mockReturnValue(dto);
+    mockPostsService.remove.mockReturnValue(dto);
+
+    expect(controller.remove('1')).toEqual(dto);
+
+    expect(mockPostsService.remove).toHaveBeenCalled();
   });
 });
--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
 
 describe('PostsService', () => {
   let service: PostsService;
 
+  const mockPostsRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockPostsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
